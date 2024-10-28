9d317560d14a742e34b3bab8a988a8cf3716b268
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-28 11:54:18 +0000

    refactor Day 1 Intro Tasks queries to use rounding

--- db/01-FirstTasksHere.sql
+++ db/01-FirstTasksHere.sql
@@ -21,14 +21,14 @@ ORDER BY date_of_birth DESC;
 
 -- Query the database to find the average rating of the movies released in the 1980s.
 
-SELECT AVG(rating) AS avg_rating_for_1980s_release
+SELECT ROUND(AVG(rating), 2) AS avg_rating_for_1980s_release
 FROM movies
 WHERE EXTRACT(YEAR FROM release_date) BETWEEN 1980 AND 1989;
 
 -- Query the database to list the locations of our customers, as well as the total and average number of loyalty points for all customers. 
 -- You can assume that if the loyalty points row is empty, they are a new customer so they should have the value set to zero.
 
-SELECT location, SUM(COALESCE(loyalty_points, 0)), AVG(COALESCE(loyalty_points, 0))
+SELECT location, SUM(COALESCE(loyalty_points, 0)), ROUND(AVG(COALESCE(loyalty_points, 0)), 2)
 FROM customers
 GROUP BY location;
 
