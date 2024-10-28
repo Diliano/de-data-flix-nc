e2136cf4eb1ddde22060cc6b86c615be6c1d1844
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-28 11:16:45 +0000

    build sql queries for Day 1 Intro Tasks

--- db/01-FirstTasksHere.sql
+++ db/01-FirstTasksHere.sql
@@ -1,9 +1,39 @@
 \c nc_flix
 
+-- Query the database to retrieve all of the movie titles that were released in the 21st century.
 
+SELECT title
+FROM movies
+WHERE EXTRACT(YEAR FROM release_date) >= 2001;
 
+-- Query the database to find the oldest customer.
 
+SELECT customer_name, date_of_birth
+FROM customers
+WHERE date_of_birth = (SELECT MIN(date_of_birth) FROM customers);
 
+-- Query the database to find the customers whose name begins with the letter D. Organise the results by age, youngest to oldest.
 
+SELECT customer_name, date_of_birth
+FROM customers
+WHERE customer_name LIKE 'D%'
+ORDER BY date_of_birth DESC;
 
+-- Query the database to find the average rating of the movies released in the 1980s.
 
+SELECT AVG(rating) AS avg_rating_for_1980s_release
+FROM movies
+WHERE EXTRACT(YEAR FROM release_date) BETWEEN 1980 AND 1989;
+
+-- Query the database to list the locations of our customers, as well as the total and average number of loyalty points for all customers. 
+-- You can assume that if the loyalty points row is empty, they are a new customer so they should have the value set to zero.
+
+SELECT location, SUM(COALESCE(loyalty_points, 0)), AVG(COALESCE(loyalty_points, 0))
+FROM customers
+GROUP BY location;
+
+-- The rise in living costs is affecting rentals. Drop the cost of all rentals by 5% and display the updated table. 
+-- As this is a monetary value, make sure it is rounded to 2 decimal places.
+
+SELECT title, cost AS old_cost, ROUND((cost * 0.95), 2) AS updated_cost
+FROM movies;
